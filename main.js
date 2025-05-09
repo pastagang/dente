d3d827b24feb47ad46a5e534e7c08b860d5e70d7
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-05-09 10:49:30 +0100

    uncomment

--- main.js
+++ main.js
@@ -7,78 +7,78 @@ import { Session } from "@flok-editor/session";
 import { flashField, evalKeymap, remoteEvalFlash } from "@flok-editor/cm-eval";
 import { UndoManager } from "yjs";
 
-// const flokBasicSetup = (doc) => {
-//   const text = doc.getText();
-//   const undoManager = new UndoManager(text);
-//   // if target is hydra, use "web" mode to evaluate on browser only, not REPLs
-//   const web = doc.target === "hydra";
+const flokBasicSetup = (doc) => {
+  const text = doc.getText();
+  const undoManager = new UndoManager(text);
+  // if target is hydra, use "web" mode to evaluate on browser only, not REPLs
+  const web = doc.target === "hydra";
 
-//   return [
-//     flashField(),
-//     remoteEvalFlash(doc),
-//     Prec.high(evalKeymap(doc, { web })),
-//     yCollab(text, doc.session.awareness, { undoManager }),
-//   ];
-// };
+  return [
+    flashField(),
+    remoteEvalFlash(doc),
+    Prec.high(evalKeymap(doc, { web })),
+    yCollab(text, doc.session.awareness, { undoManager }),
+  ];
+};
 
-// const createEditor = (doc) => {
-//   const state = EditorState.create({
-//     doc: doc.content,
-//     extensions: [
-//       basicSetup,
-//       flokBasicSetup(doc),
-//       javascript(),
-//       EditorView.lineWrapping,
-//       oneDark,
-//     ],
-//   });
+const createEditor = (doc) => {
+  const state = EditorState.create({
+    doc: doc.content,
+    extensions: [
+      // basicSetup,
+      // flokBasicSetup(doc),
+      // javascript(),
+      // EditorView.lineWrapping,
+      // oneDark,
+    ],
+  });
 
-//   const editorEl = document.querySelector(`#${doc.id} .editor`);
-//   const view = new EditorView({
-//     state,
-//     parent: editorEl,
-//   });
+  const editorEl = document.querySelector(`#${doc.id} .editor`);
+  const view = new EditorView({
+    state,
+    parent: editorEl,
+  });
 
-//   const targetEl = document.querySelector(`#${doc.id} .target`);
-//   targetEl.value = doc.target;
+  const targetEl = document.querySelector(`#${doc.id} .target`);
+  targetEl.value = doc.target;
 
-//   targetEl.addEventListener("change", (e) => {
-//     doc.target = e.target.value;
-//   });
-//   doc.session.on(`change-target:${doc.id}`, () => {
-//     targetEl.value = doc.target;
-//   });
+  targetEl.addEventListener("change", (e) => {
+    doc.target = e.target.value;
+  });
+  doc.session.on(`change-target:${doc.id}`, () => {
+    targetEl.value = doc.target;
+  });
 
-//   return [state, view];
-// };
+  return [state, view];
+};
 
-// const handleMessage = (msg) => {
-//   console.log("message", msg);
-// };
+const handleMessage = (msg) => {
+  console.log("message", msg);
+};
 
-// const handleEvalHydra = (msg) => {
-//   console.log("eval:hydra", msg);
-//   // evaluate hydra code here...
-// };
+const handleEvalHydra = (msg) => {
+  console.log("eval:hydra", msg);
+  // evaluate hydra code here...
+};
 
-// const session = new Session("default", { port: 3000 });
-// window.session = session;
+const session = new Session("default", { port: 3000 });
+window.session = session;
 
-// session.on("change", (...args) => console.log("change", ...args));
-// session.on("message", handleMessage);
-// session.on("eval:hydra", handleEvalHydra);
+session.on("change", (...args) => console.log("change", ...args));
+session.on("message", handleMessage);
+session.on("eval:hydra", handleEvalHydra);
 
-// session.on("sync", () => {
-//   // If session is empty, create two documents
-//   if (session.getDocuments().length === 0) {
-//     session.setActiveDocuments([
-//       { id: "slot1", target: "tidal" },
-//       { id: "slot2", target: "hydra" },
-//     ]);
-//   }
+session.on("sync", () => {
+  // If session is empty, create two documents
+  if (session.getDocuments().length === 0) {
+    session.setActiveDocuments([
+      { id: "slot1", target: "tidal" },
+      { id: "slot2", target: "hydra" },
+    ]);
+  }
 
-//   // Create editors for each document
-//   session.getDocuments().map((doc) => createEditor(doc));
-// });
+  // Create editors for each document
+  session.getDocuments().map((doc) => createEditor(doc));
+});
 
-// session.initialize();
+session.initialize();
