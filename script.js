e22ebc87a8635a2df3a6a04bd5417da902a08ab3
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-05-09 15:15:00 +0100

    comments

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const session = new Session("pastagang5", {
 });
 
 session.on("change", (...args) => {
-  console.log("change", ...args);
+  // console.log("change", ...args);
 });
 session.on("eval:hydra", (msg) => {
-  console.log("eval:hydra", msg);
+  // console.log("eval:hydra", msg);
 });
 session.on("eval:strudel", (msg) => {
-  console.log("eval:strudel", msg);
+  // console.log("eval:strudel", msg);
 });
 
 session.on("sync", () => {
@@ -21,7 +21,42 @@ session.on("sync", () => {
   for (const flokDocument of flokDocuments) {
     const editorElement = createEditorElement(flokDocument);
     session._yText(flokDocument.id).observe((textEvent) => {
+      const changes = textEvent.changes;
+
+      // Figure out what the changes do
+      let retainCount = 0;
+      let insertCount = 0;
+      let deleteCount = 0;
+      for (const operation of changes.delta) {
+        if (operation.retain) {
+          if (retainCount) throw new Error("Unexpected double retain");
+          retainCount += operation.retain;
+        }
+        if (operation.insert) {
+          if (insertCount) throw new Error("Unexpected double insert");
+          insertCount += operation.insert.length;
+        }
+        if (operation.delete) {
+          if (deleteCount) throw new Error("Unexpected double delete");
+          deleteCount += operation.delete;
+        }
+      }
+
+      // Figure out where the new selection should go
+      let selectionStart = editorElement.selectionStart;
+      let selectionEnd = editorElement.selectionEnd;
+      if (selectionStart > retainCount) {
+        selectionStart = Math.max(selectionStart - deleteCount, retainCount);
+        selectionStart += insertCount;
+      }
+      if (selectionEnd > retainCount) {
+        selectionEnd = Math.max(selectionEnd - deleteCount, retainCount);
+        selectionEnd += insertCount;
+      }
+
+      // Update the editor
       editorElement.value = flokDocument.getText();
+      editorElement.setSelectionRange(selectionStart, selectionEnd);
     });
   }
 });
@@ -42,3 +77,4 @@ function createEditorElement(doc) {
 }
 
 session.initialize();
+window["session"] = session;
